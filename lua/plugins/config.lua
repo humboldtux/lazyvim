f778fac293f2379e628d074131fa7bfb21e40c26
Benoit Benedetti <benoit.benedetti@gmail.com>
2025-02-09 18:36:49 +0100

    Extra plugins

---
+++ lua/plugins/config.lua
@@ -0,0 +1,13 @@
+return {
+
+  { import = "lazyvim.plugins.extras.ai.copilot" },
+  { import = "lazyvim.plugins.extras.lang.git" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.php" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.lang.sql" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.yaml" },
+}
