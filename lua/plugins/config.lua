d190d004acb597df332c7aa7eeaf4d0e4e5174c9
Benoit Benedetti <benoit.benedetti@gmail.com>
2026-03-21 18:24:07 +0100

    Fix dap

--- lua/plugins/config.lua
+++ lua/plugins/config.lua
@@ -48,17 +48,8 @@ return {
       "nvim-lua/plenary.nvim",
     },
     keys = {
-      {
-        "<leader>-",
-        mode = { "n", "v" },
-        "<cmd>Yazi<cr>",
-        desc = "Open Yazi",
-      },
-      {
-        "<leader>cw",
-        "<cmd>Yazi cwd<cr>",
-        desc = "Yazi (cwd)",
-      },
+      { "<leader>-", mode = { "n", "v" }, "<cmd>Yazi<cr>", desc = "Open Yazi" },
+      { "<leader>cw", "<cmd>Yazi cwd<cr>", desc = "Yazi (cwd)" },
     },
     opts = {
       open_for_directories = false,
@@ -66,7 +57,7 @@ return {
   },
 
   -- ========================
-  -- Treesitter (langages utiles)
+  -- Treesitter
   -- ========================
   {
     "nvim-treesitter/nvim-treesitter",
@@ -86,7 +77,7 @@ return {
   },
 
   -- ========================
-  -- Mason (tools système)
+  -- Mason (LSP + DAP)
   -- ========================
   {
     "mason-org/mason.nvim",
@@ -104,7 +95,162 @@ return {
         "shellcheck",
         "yamllint",
         "ansible-lint",
+
+        -- DAP
+        "debugpy",
+        "bash-debug-adapter",
+        "js-debug-adapter",
       },
     },
+  },
+
+  -- ========================
+  -- DAP Core
+  -- ========================
+  {
+    "mfussenegger/nvim-dap",
+    keys = {
+      {
+        "<F5>",
+        function()
+          require("dap").continue()
+        end,
+        desc = "DAP Continue",
+      },
+      {
+        "<F10>",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "DAP Step Over",
+      },
+      {
+        "<F11>",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "DAP Step Into",
+      },
+      {
+        "<F12>",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "DAP Step Out",
+      },
+      {
+        "<leader>b",
+        function()
+          require("dap").toggle_breakpoint()
+        end,
+        desc = "Breakpoint",
+      },
+      {
+        "<leader>B",
+        function()
+          require("dap").set_breakpoint(vim.fn.input("Condition: "))
+        end,
+        desc = "Conditional Breakpoint",
+      },
+    },
+  },
+
+  -- ========================
+  -- DAP UI
+  -- ========================
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dapui.setup()
+
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+
+  -- ========================
+  -- Python Debug
+  -- ========================
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    config = function()
+      require("dap-python").setup("python")
+    end,
+  },
+
+  -- ========================
+  -- Go Debug (Delve)
+  -- ========================
+  {
+    "mfussenegger/nvim-dap",
+    ft = "go",
+    config = function()
+      local dap = require("dap")
+
+      dap.adapters.go = {
+        type = "server",
+        port = "${port}",
+        executable = {
+          command = "dlv",
+          args = { "dap", "-l", "127.0.0.1:${port}" },
+        },
+      }
+
+      dap.configurations.go = {
+        {
+          type = "go",
+          name = "Debug",
+          request = "launch",
+          program = "${file}",
+        },
+        {
+          type = "go",
+          name = "Debug test",
+          request = "launch",
+          mode = "test",
+          program = "${file}",
+        },
+      }
+    end,
+  },
+
+  -- ========================
+  -- Bash Debug
+  -- ========================
+  {
+    "mfussenegger/nvim-dap",
+    ft = "sh",
+    config = function()
+      local dap = require("dap")
+
+      dap.adapters.sh = {
+        type = "executable",
+        command = "bash-debug-adapter",
+      }
+
+      dap.configurations.sh = {
+        {
+          type = "sh",
+          request = "launch",
+          name = "Launch Bash script",
+          program = "${file}",
+        },
+      }
+    end,
   },
 }
